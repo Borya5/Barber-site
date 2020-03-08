0846dce54749e315930ad882655b75016b3b3863
Borya5 <miron.vasya21@yandex.ru>
2020-03-08 12:59:15 +0300

    Добавляют логику появления и скытия модальногокна

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var modalOpenButton = document.querySelector(".slider_btn--modalShow");
+var modal = document.querySelector('.modal');
+modalOpenButton.addEventListener('click', function () {
+
+});
